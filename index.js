2fa501aa25e5da81cd86a07611160612a5efdc1e
RHG <huskey.rusty@gmail.com>
2017-11-27 19:02:19 -0500

    Bugfix Again..

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
-const YTDL = require("ytdl-core")
 const prefix = require("../config.json").prefix
 const ytapi = process.env.ytapikey
 const search = require('youtube-search');
-const Discord = require('discord.js');
 const YTDL = require("ytdl-core")
 const FFMPEG = require("ffmpeg");
 
