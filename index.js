32e8f53b3b96a0dbdd28138260a6973875c9465d
RHG <huskey.rusty@gmail.com>
2017-11-27 19:00:18 -0500

    BugFixes for everyone!

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const YTDL = require("ytdl-core")
 const prefix = require("../config.json").prefix
 const ytapi = process.env.ytapikey
 const search = require('youtube-search');
+const Discord = require('discord.js');
+const YTDL = require("ytdl-core")
 const FFMPEG = require("ffmpeg");
 
 
@@ -26,9 +28,13 @@ function play(connect, msg, bot) {
       .setThumbnail(info.iurlmq)
       .setDescription(`I will now start playing **${info.title}** in ${connect.channel.name}\n\n**By: **${info.author.name}\n**Requested By:** ${msg.author.tag}`)
 
-    msg.channel.send({embed: em}).then(m => m.delete(35000))
+    msg.channel.send({
+      embed: em
+    }).then(m => m.delete(35000))
   });
-  server.dispatcher = connect.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+  server.dispatcher = connect.playStream(YTDL(server.queue[0], {
+    filter: "audioonly"
+  }));
 
   server.queue.shift();
 
@@ -42,69 +48,73 @@ function play(connect, msg, bot) {
         .setColor("7289DA")
         .setAuthor(`I have now stopped playing in ${connect.channel.name}`)
 
-      msg.channel.send({embed: em}).then(m => m.delete(25000))
+      msg.channel.send({
+        embed: em
+      }).then(m => m.delete(25000))
     }
   })
 };
 
 function removedat(msg) {
- if (msg.channel.type === "dm") return;
- if (!msg.deletable){
-   console.log(`[DEL] Couldn't remove a message in ${msg.guild.name}`)
-   return;
- }
- msg.delete();
+  if (msg.channel.type === "dm") return;
+  if (!msg.deletable) {
+    console.log(`[DEL] Couldn't remove a message in ${msg.guild.name}`)
+    return;
+  }
+  msg.delete();
   console.log(`[DEL] Removed a message in ${msg.guild.name}`)
 }
 
-function errorhandle(err){
+function errorhandle(err) {
   console.log(`[ERROR] ${err}`)
 }
 
 console.log(`Now loading RBot Music...`)
 
-bot.on("message", function(message){
+bot.on("message", function(message) {
   if (message.author.equals(bot.user)) return;
-   if (message.author.bot) return;
-   if (!message.content.startsWith(`${prefix}music`)) return;
+  if (message.author.bot) return;
+  if (!message.content.startsWith(`${prefix}music`)) return;
 
-   var args = message.content.substring(prefix.length+6).split(" ");
+  var args = message.content.substring(prefix.length + 6).split(" ");
 
-   console.log(args)
-   switch (args[0].toLowerCase()) {
-     case "help":
-       removedat(message)
-       let em = new Discord.RichEmbed()
-         .setColor("7289DA")
-         .setAuthor(`${bot.user.username} Music`, bot.user.avatarURL)
-         .setDescription("Fine I'm simply a simple musicbot.")
-         .addField(`${prefix}help`, `Sends this message.`)
-         .addField(`${prefix}play (Youtube link) or (field)`, `Plays a song in the current channel.`)
-         .addField(`${prefix}skip`, `Skips the current song`)
-         .addField(`${prefix}stop`, `Skips the current songStops and ends everything`)
+  console.log(args)
+  switch (args[0].toLowerCase()) {
+    case "help":
+      removedat(message)
+      let em = new Discord.RichEmbed()
+        .setColor("7289DA")
+        .setAuthor(`${bot.user.username} Music`, bot.user.avatarURL)
+        .setDescription("Fine I'm simply a simple musicbot.")
+        .addField(`${prefix}help`, `Sends this message.`)
+        .addField(`${prefix}play (Youtube link) or (field)`, `Plays a song in the current channel.`)
+        .addField(`${prefix}skip`, `Skips the current song`)
+        .addField(`${prefix}stop`, `Skips the current songStops and ends everything`)
 
-       message.channel.send({embed: em});
-       break;
-     case "play":
-     if (!message.member.voiceChannel) {
-       removedat(message)
-       message.channel.send(":x: Oh I forgot.. You need to be in a voice channel!")
-       break;
-     }
-     if (!message.member.voiceChannel.joinable || message.member.voiceChannel.full) {
-       removedat(message)
-       message.channel.send(":x: Looks like I cannot join that voice channel.")
-       break;
-     }
-       if (!args[1]) {
-         removedat(message)
-         message.channel.send(":x: Umm where's the link?")
-         break;
-       }
-       if (!YTDL.validateURL(args[1])) {
+      message.channel.send({
+        embed: em
+      });
+      break;
+    case "play":
+      if (!message.member.voiceChannel) {
+        removedat(message)
+        message.channel.send(":x: Oh I forgot.. You need to be in a voice channel!")
+        break;
+      }
+      if (!message.member.voiceChannel.joinable || message.member.voiceChannel.full) {
+        removedat(message)
+        message.channel.send(":x: Looks like I cannot join that voice channel.")
+        break;
+      }
+      if (!args[1]) {
+        removedat(message)
+        message.channel.send(":x: Umm where's the link?")
+        break;
+      }
+      if (!YTDL.validateURL(args[1])) {
         // message.channel.send(":x: Are you sure thats a Youtube link?")
-         search(args.slice(1).join(" "), opts, function(err, results) {
-          if(err) return console.log(err);
+        search(args.slice(1).join(" "), opts, function(err, results) {
+          if (err) return console.log(err);
           if (!servers[message.guild.id]) servers[message.guild.id] = {
             queue: []
           };
@@ -119,83 +129,80 @@ bot.on("message", function(message){
             play(connection, message, bot);
           })
           removedat(message)
-         });
-       } else {
-         if (!servers[message.guild.id]) servers[message.guild.id] = {
-           queue: []
-         };
+        });
+      } else {
+        if (!servers[message.guild.id]) servers[message.guild.id] = {
+          queue: []
+        };
 
-         console.log(`[QUEUE] Added music to ${message.guild.name}'s queue!' `)
+        console.log(`[QUEUE] Added music to ${message.guild.name}'s queue!' `)
 
-         var server = servers[message.guild.id]
+        var server = servers[message.guild.id]
 
-         server.queue.push(args[1]);
+        server.queue.push(args[1]);
 
-         if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
-           play(connection, message, bot);
-         })
-         removedat(message)
-       }
+        if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+          play(connection, message, bot);
+        })
+        removedat(message)
+      }
 
-       break;
-     case "skip":
-       removedat(message)
-       var server = servers[message.guild.id];
-       if (server.dispatcher) server.dispatcher.end()
-       break;
-     case "stop":
-       removedat(message)
-       var server = servers[message.guild.id];
-       if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
-       break;
-     case "leave":
-       removedat(message)
-       if (!message.member.voiceChannel) {
-         message.channel.send(":x: Umm you need to join a channel to be able to use this command...")
-         break;
-       };;
-       if (!message.guild.voiceConnection.channel.name == message.member.voiceChannel.name) {
-         message.channel.send(":x: Umm you need to join my channel to use this.")
-         break;
-       };
-       if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
-       break;
-     case "join":
-       removedat(message)
+      break;
+    case "skip":
+      removedat(message)
+      var server = servers[message.guild.id];
+      if (server.dispatcher) server.dispatcher.end()
+      break;
+    case "stop":
+      removedat(message)
+      var server = servers[message.guild.id];
+      if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+      break;
+    case "leave":
+      removedat(message)
+      if (!message.member.voiceChannel) {
+        message.channel.send(":x: Umm you need to join a channel to be able to use this command...")
+        break;
+      };;
+      if (!message.guild.voiceConnection.channel.name == message.member.voiceChannel.name) {
+        message.channel.send(":x: Umm you need to join my channel to use this.")
+        break;
+      };
+      if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+      break;
+    case "join":
+      removedat(message)
 
-       if (!message.member.voiceChannel) {
-         message.channel.send(":x: Umm you need to join a channel to be able to use this command...")
-         break;
-       };;
-       if (!message.member.voiceChannel.joinable || message.member.voiceChannel.full) {
-         message.channel.send(":x: Looks like I cannot join that voice channel.")
-         break;
-       }
-       message.member.voiceChannel.join()
-       break;
-     case "l":
-       if (!message.author.id == mastid) {
-         console.log(`[LEAVE ATTEMPT] ${message.author.username}#${message.author.discriminator} | ${message.guild.name} | ${message.channel.name}`);
-         break;
-       }
-       removedat(message)
-       bot.guilds.forEach(async (guil, id) => {
-         if (guil.id == args[1]) {
-           guil.leave()
-           return;
-         }
-       });
-       console.log(`[MANUAL LEAVE] Guild not found!`);
-       message.channel.send("Guild wasn't found in my list!")
-       break;
-   };
-   //message.delete();
-
+      if (!message.member.voiceChannel) {
+        message.channel.send(":x: Umm you need to join a channel to be able to use this command...")
+        break;
+      };;
+      if (!message.member.voiceChannel.joinable || message.member.voiceChannel.full) {
+        message.channel.send(":x: Looks like I cannot join that voice channel.")
+        break;
+      }
+      message.member.voiceChannel.join()
+      break;
+    case "l":
+      if (!message.author.id == mastid) {
+        console.log(`[LEAVE ATTEMPT] ${message.author.username}#${message.author.discriminator} | ${message.guild.name} | ${message.channel.name}`);
+        break;
+      }
+      removedat(message)
+      bot.guilds.forEach(async (guil, id) => {
+        if (guil.id == args[1]) {
+          guil.leave()
+          return;
+        }
+      });
+      console.log(`[MANUAL LEAVE] Guild not found!`);
+      message.channel.send("Guild wasn't found in my list!")
+      break;
+  };
+  //message.delete();
 });
 
-bot.on("error", function(err){
-
-bot.on("ready", function(){
+bot.on("ready", function() {
   // Main Print
   console.log(`~~~~~~~~~~~~~  ${bot.user.username}  ~~~~~~~~~~~~~~`);
   console.log(``);
